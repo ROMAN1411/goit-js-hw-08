a7574375f3c89e5ab858cbc56f56e247f931f04e
ROMAN BEZNOSIUK <89782854+ROMAN1411@users.noreply.github.com>
2022-02-08 13:42:21 +0200

    Сompleted 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+function makeGallery(items) {
+    items.map((item) => {
+        const markup =
+            `<div class="gallery__item">
+                <a class="gallery__link" href="${item.original}">
+                    <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    data-source="${item.original}"
+                    alt="${item.description}"
+                    />
+                </a>
+            </div>`
+
+        galleryList.insertAdjacentHTML("beforeend", markup);
+    })
+}
+makeGallery(galleryItems)
+console.log(galleryList)
+
+new SimpleLightbox('.gallery__link', {
+    captionsData: "alt",
+    captionDelay: 250,
+    overlayOpacity: 1,
+});
