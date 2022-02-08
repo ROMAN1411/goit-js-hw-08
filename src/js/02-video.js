7997a585b308c5146ad979bb3eaefc0257a87826
ROMAN BEZNOSIUK <89782854+ROMAN1411@users.noreply.github.com>
2022-02-08 22:17:02 +0200

    Сompleted 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+
+const TIME__SET = "videoplayer-current-time";
+const localTime = localStorage.getItem(TIME__SET);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const throttle = require('lodash.throttle');
+
+if (localTime) {
+  player.setCurrentTime(localTime);
+}
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate({ seconds }) {
+  localStorage.setItem(TIME__SET, seconds);
+}
 
