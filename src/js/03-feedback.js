cd22ea2efb959bbffe3a13302dfca2ca96a5177f
ROMAN BEZNOSIUK <89782854+ROMAN1411@users.noreply.github.com>
2022-02-09 14:59:58 +0200

    Сompleted 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash/throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const formValues = {
+  email: form.email.value,
+  message: form.message.value,
+};
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput({ target: { name, value } }) {
+  formValues[name] = value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formValues));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  const {
+    elements: { email, message }
+  } = evt.target;
+
+  if (email.value === "" || message.value === "") {
+    alert("All fields must be filled!");
+    return
+  }
+  console.log('Submitting the form!');
+  console.log(`Email: ${email.value}`);
+  console.log(`Password: ${message.value}`);
+  localStorage.removeItem(STORAGE_KEY);
+  evt.currentTarget.reset();
+};
+
+populateTextarea();
+
+function populateTextarea() {
+  const savedForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedForm === null) {
+    return;
+  }
+  form.email.value = savedForm.email;
+  form.message.value = savedForm.message;
+};
