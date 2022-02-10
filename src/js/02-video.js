4f5d5b28be74d497251cb4faffe15bfcfdbcd11e
ROMAN BEZNOSIUK <89782854+ROMAN1411@users.noreply.github.com>
2022-02-10 23:10:15 +0200

    Mistake correction

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,12 +7,25 @@ const player = new Player(iframe);
 
 const throttle = require('lodash.throttle');
 
-if (localTime) {
-  player.setCurrentTime(localTime);
-}
-player.on('timeupdate', throttle(onTimeUpdate, 1000));
-
 function onTimeUpdate({ seconds }) {
   localStorage.setItem(TIME__SET, seconds);
 }
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+if (localTime) {
+  player.setCurrentTime(localTime)
+    .then(function (seconds) { })
+    .catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
+}
+
+
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,12 +8,15 @@ const formValues = {
   message: form.message.value,
 };
 
+
 form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
 
 function onFormInput({ target: { name, value } }) {
   formValues[name] = value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formValues));
+
+  // console.log(localStorage)
 }
 
 function onFormSubmit(evt) {
@@ -21,27 +24,43 @@ function onFormSubmit(evt) {
 
   const {
     elements: { email, message }
-  } = evt.target;
+  } = evt.currentTarget;
 
   if (email.value === "" || message.value === "") {
     alert("All fields must be filled!");
+    evt.currentTarget.reset();
     return
   }
+  
   console.log('Submitting the form!');
   console.log(`Email: ${email.value}`);
   console.log(`Password: ${message.value}`);
+  // console.log(formValues)
+
   localStorage.removeItem(STORAGE_KEY);
   evt.currentTarget.reset();
 };
 
 populateTextarea();
 
-function populateTextarea() {
-  const savedForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+// function populateTextarea() {
+//   const savedForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+//   if (savedForm === null) {
+//     return;
+//   }
 
-  if (savedForm === null) {
-    return;
+//   form.email.value = savedForm.email;
+//   form.message.value = savedForm.message;
+// };
+
+
+function populateTextarea() {
+  let savedForm = localStorage.getItem(STORAGE_KEY);
+  if (savedForm) {
+    savedForm = JSON.parse(savedForm);
+    Object.entries(savedForm).forEach(([key, value]) => {
+      formValues[key] = value;
+      form.elements[key].value = value;
+    });
   }
-  form.email.value = savedForm.email;
-  form.message.value = savedForm.message;
-};
+}
